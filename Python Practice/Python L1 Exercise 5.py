0b5d0a8a925ec12b109e079a340d6f3196a65b9f
wemmanuel-emagine <126636621+wemmanuel-emagine@users.noreply.github.com>
2023-03-01 15:17:47 -0400

    Delete Python Practice directory

--- Python Practice/Python L1 Exercise 5.py
+++
@@ -1,8 +0,0 @@
-# objective:
-# Write a function that converts hours into seconds.
-
-def how_many_seconds(hours):
-    seconds = hours * 3600
-    print(seconds)
-
-how_many_seconds(2)
--- Python Practice/Python L1 Exercise 7.py
+++
@@ -1,7 +0,0 @@
-# objective:
-# There is a single operator in Python, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
-
-def reminder(num1,num2):
-   return num1 % num2
-
-print(reminder(1,3))
